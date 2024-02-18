ef1915a960f6673e05c9f9a99c0d29ff59a5d5c2
Steve Yeager <steve.f.yeager@gmail.com>
2024-02-18 14:37:03 -0800

    Add Poll

---
+++ Pointocracy.Core/Models/Poll.cs
@@ -0,0 +1,10 @@
+﻿namespace Pointocracy.Core.Models;
+
+public class Poll(
+    Guid id,
+    string name,
+    string? description,
+    PollState state,
+    VoteRules voteRules,
+    IEnumerable<Option> options,
+    PollResult? result = null);
