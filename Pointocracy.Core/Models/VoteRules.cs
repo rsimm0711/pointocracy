6c3ef014f1027e3d778ce75627f3910e8bc9cb65
Steve Yeager <steve.f.yeager@gmail.com>
2024-02-18 16:42:32 -0800

    Add endpoint and domain action for creating a new poll

--- Pointocracy.Core/Models/VoteRules.cs
+++ Pointocracy.Core/Models/VoteRules.cs
@@ -1,3 +1,6 @@
 ﻿namespace Pointocracy.Core.Models;
 
-public sealed record VoteRules(uint VotesPositive, uint VotesNegative, uint LimitPositive, uint LimitNegative);
+public sealed record VoteRules(uint VotesPositive, uint VotesNegative, uint LimitPositive, uint LimitNegative)
+{
+    public static VoteRules Default = new(3, 2, 3, 2);
+}
