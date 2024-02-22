510f12ee89669fc967775367c11c1bf7366a93f4
Steve Yeager <steve.f.yeager@gmail.com>
2024-02-21 21:42:20 -0800

    Add SaveContext

---
+++ Pointocracy.Infra/DataCommands/SaveContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Pointocracy.Infra.DataCommands;
+
+public interface ISaveContext
+{
+    ValueTask SaveAsync();
+}
+
+internal sealed class SaveContext(PointocracyDb database) : ISaveContext
+{
+    public async ValueTask SaveAsync() => await database.SaveChangesAsync();
+}
