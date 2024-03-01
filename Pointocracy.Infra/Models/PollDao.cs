90f01cac942bb44d2207d46c0780aca9c23ade52
Steve Yeager <steve.f.yeager@gmail.com>
2024-03-01 13:33:50 -0800

    Add Name and State to PollDao

--- Pointocracy.Infra/Models/PollDao.cs
+++ Pointocracy.Infra/Models/PollDao.cs
@@ -1,6 +1,10 @@
-﻿namespace Pointocracy.Infra.Models;
+using Pointocracy.Core.Models;
+
+namespace Pointocracy.Infra.Models;
 
 internal sealed class PollDao
 {
     public required Guid Id { get; init; }
+    public string Name { get; init; } = null!;
+    public PollState State { get; init; }
 }
