ed24571410c0bb3fe01be261f0aadce2cbd3a85f
Steve Yeager <steve.f.yeager@gmail.com>
2024-03-01 13:34:46 -0800

    Add FindPollQuery

---
+++ Pointocracy.Infra/Queries/FindPollQuery.cs
@@ -0,0 +1,15 @@
+﻿using Pointocracy.Core.Models;
+using Pointocracy.Core.Queries;
+using Pointocracy.Infra.Models;
+
+namespace Pointocracy.Infra.Queries;
+
+internal sealed class FindPollQuery(PointocracyDb database) : IFindPollQuery
+{
+    public async ValueTask<Poll> FindAsync(PollId id)
+    {
+        var dao = await database.FindAsync<PollDao>(id.Guid);
+        var poll = PollDao.ToPoll(dao);
+        return poll;
+    }
+}
